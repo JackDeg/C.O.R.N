9b0c6ec5223bec70e16606f04e58b22554db7bd5
JackDeg <jack.degnen@gmail.com>
2020-05-02 04:16:42 +0100

    MF overhaul
    Redoing pills (all broken atm)
    added agricultural mind, precursors and surgeries

---
+++ Source/CORN/CORN/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace CORN
+{
+    [StaticConstructorOnStartup]
+    public static class CornMod
+    {
+        static CornMod()
+        {
+            Logr.Info("Hello World!");
+        }
+    }
+}
---
+++ Source/CORN/CORN/Logr.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace CORN
+{
+     class Logr
+    {
+        public static void Info(string msg)
+        {
+            Log.Message($"[C.O.R.N]: [INFO]: {msg}");
+        }
+        public static void Warn(string msg)
+        {
+            Log.Warning($"[C.O.R.N]: [WARN]: {msg}");
+        }
+        public static void Severe(string msg)
+        {
+            Log.Error($"[C.O.R.N]: [SEVERE]: {msg}");
+        }
+    }
+}
